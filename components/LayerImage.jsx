503c3493ad7918400c2da9220b67ba03f804f278
Ruben <rr@headwire.com>
2022-09-29 14:28:53 -0700

    Update LayerImage.jsx

--- components/LayerImage.jsx
+++ components/LayerImage.jsx
@@ -14,7 +14,7 @@ export default function LayerImage({ data, panelNr }) {
         id={id}
         loading={(panelNr === 0 || forceLoad) ? "eager" : "lazy"}
         className="image"
-        src={image?._authorUrl}
+        src={image?._publishUrl}
         alt={altText?.plaintext}
       />
     </div>
